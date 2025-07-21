7bf570cf7c9e99f2f5cacb0ebf6a9c46da38266d
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-07-21 23:01:13 +0900

    feat: database connection implementation is added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
+use std::env;
+
+use crate::article::Article;
+use sqlx::PgPool;
+
+pub mod article;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // get database url from environment variable
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or("postgres://user:password@localhost:15432/app_db".to_string());
+
+    // establish connection pool
+    let db_pool = PgPool::connect(&database_url).await?;
+
+    // test database connection
+    let s: Vec<Article> = sqlx::query_as(
+        r#"
+        SELECT id, title, body, created_at FROM articles
+        "#,
+    )
+    .fetch_all(&db_pool)
+    .await?;
+
+    s.iter()
+        .flat_map(|v| serde_json::to_string(&v))
+        .for_each(|v| println!("{v}"));
+
     Ok(())
 }
